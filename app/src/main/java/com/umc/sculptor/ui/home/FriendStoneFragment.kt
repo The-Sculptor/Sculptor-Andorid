dded64eb261333ab2dc2e3c6981afe41d43e9915
mmihye <smilehye910@naver.com>
2024-02-06 04:31:46 +0900

    [#18] feat: 친구돌 페이지 ui

---
+++ app/src/main/java/com/umc/sculptor/ui/home/FriendStoneFragment.kt
@@ -0,0 +1,38 @@
+package com.umc.sculptor.ui.home
+
+import android.annotation.SuppressLint
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginRight
+import com.umc.sculptor.MainActivity
+import com.umc.sculptor.R
+import com.umc.sculptor.base.BaseFragment
+import com.umc.sculptor.databinding.FragmentHomeFriendStoneBinding
+import com.umc.sculptor.databinding.FragmentMypageBinding
+
+class FriendStoneFragment : BaseFragment<FragmentHomeFriendStoneBinding>(R.layout.fragment_home_friend_stone) {
+
+    override fun initStartView() {
+        super.initStartView()
+        (activity as MainActivity).hideBottomNav(true)
+        (activity as MainActivity).hideIconAndShowBack(true)
+
+    }
+
+    override fun initDataBinding() {
+        super.initDataBinding()
+
+
+    }
+
+
+    override fun initAfterBinding() {
+        super.initAfterBinding()
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        (activity as MainActivity).hideIconAndShowBack(false)
+    }
+}
