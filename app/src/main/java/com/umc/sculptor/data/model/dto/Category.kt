092cee0ee6c297e7f97dd44478a978bd8a5cabe3
mmihye <smilehye910@naver.com>
2024-02-05 23:09:34 +0900

    [#5] feat : stoneViewModel

---
+++ app/src/main/java/com/umc/sculptor/data/model/dto/Category.kt
@@ -0,0 +1,7 @@
+package com.umc.sculptor.data.model.dto
+
+enum class Category {
+    HEALTH,
+    STUDY,
+    ETC
+}
--- app/src/main/java/com/umc/sculptor/ui/workshop/createStone/RockCategoryFragment.kt
+++ app/src/main/java/com/umc/sculptor/ui/workshop/createStone/RockCategoryFragment.kt
@@ -1,13 +1,23 @@
 package com.umc.sculptor.ui.workshop.createStone
 
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import com.umc.sculptor.MainActivity
 import com.umc.sculptor.R
 import com.umc.sculptor.base.BaseFragment
+import com.umc.sculptor.data.model.dto.Category
+import com.umc.sculptor.data.model.dto.NewStoneViewModel
 import com.umc.sculptor.databinding.FragmentCreateRockCategoryBinding
 import com.umc.sculptor.databinding.FragmentHomeBinding
 import com.umc.sculptor.databinding.FragmentWorkshopBinding
 
 class RockCategoryFragment : BaseFragment<FragmentCreateRockCategoryBinding>(R.layout.fragment_create_rock_category) {
+    val viewModel: NewStoneViewModel by lazy {
+        ViewModelProvider(requireActivity()).get(NewStoneViewModel::class.java)
+    }
+    var currentIndex : Int = 0
+    lateinit var category : Category
 
     override fun initStartView() {
         super.initStartView()
@@ -18,15 +28,78 @@ class RockCategoryFragment : BaseFragment<FragmentCreateRockCategoryBinding>(R.l
         super.initDataBinding()
 
 
+        binding.tvStoneName.text = viewModel.getNewStone().value?.name ?: "null"
+
     }
 
 
     override fun initAfterBinding() {
         super.initAfterBinding()
 
+        binding.tvCategoryEtc.setOnClickListener {
+            if(currentIndex == 3)
+                currentIndex = 0
+            else
+                currentIndex = 3
+            setCategoryBackground()
+        }
+
+        binding.tvCategoryHealth.setOnClickListener {
+            if(currentIndex == 1)
+                currentIndex = 0
+            else
+                currentIndex = 1
+            setCategoryBackground()
+        }
+
+        binding.tvCategoryStudy.setOnClickListener {
+            if(currentIndex == 2)
+                currentIndex = 0
+            else
+                currentIndex = 2
+            setCategoryBackground()
+        }
+
         binding.btnComplete.setOnClickListener {
+            viewModel.setNewStoneCategory(category)
             navController.navigate(R.id.action_rockCategoryFragment_to_rockGoalFragment)
         }
 
+    }
+
+    private fun setCategoryBackground(){
+        when(currentIndex){
+            1 -> {
+                category = Category.HEALTH
+                binding.tvCategoryStudy.setBackgroundResource(R.drawable.round_4_rectangle_purple)
+                binding.tvCategoryHealth.setBackgroundResource(R.drawable.round_4_rectangle_purple_border)
+                binding.tvCategoryEtc.setBackgroundResource(R.drawable.round_4_rectangle_purple)
+            }
+            2 -> {
+                category = Category.STUDY
+                binding.tvCategoryStudy.setBackgroundResource(R.drawable.round_4_rectangle_purple_border)
+                binding.tvCategoryHealth.setBackgroundResource(R.drawable.round_4_rectangle_purple)
+                binding.tvCategoryEtc.setBackgroundResource(R.drawable.round_4_rectangle_purple)
+            }
+            3 -> {
+                category = Category.ETC
+                binding.tvCategoryStudy.setBackgroundResource(R.drawable.round_4_rectangle_purple)
+                binding.tvCategoryHealth.setBackgroundResource(R.drawable.round_4_rectangle_purple)
+                binding.tvCategoryEtc.setBackgroundResource(R.drawable.round_4_rectangle_purple_border)
+            }
+            else ->{
+                binding.tvCategoryStudy.setBackgroundResource(R.drawable.round_4_rectangle_purple)
+                binding.tvCategoryHealth.setBackgroundResource(R.drawable.round_4_rectangle_purple)
+                binding.tvCategoryEtc.setBackgroundResource(R.drawable.round_4_rectangle_purple)
+            }
+        }
+
+        if (currentIndex > 0) {
+            binding.btnComplete.isEnabled = true
+            binding.btnComplete.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+        } else {
+            binding.btnComplete.isEnabled = false
+            binding.btnComplete.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray_908F90))
+        }
     }
 }
