bf97fb9cff8e2ec8118bf7b936307b9d36602685
kongsumin <sm3904@naver.com>
2024-02-21 05:16:35 +0900

    착용중인 상품 아이템 img

--- app/src/main/java/com/umc/sculptor/ui/store/ItemWearingRVAdapter.kt
+++ app/src/main/java/com/umc/sculptor/ui/store/ItemWearingRVAdapter.kt
@@ -48,14 +48,19 @@ class ItemWearingRVAdapter(itemList: ArrayList<DataXXXX>): RecyclerView.Adapter<
     override fun getItemCount(): Int = itemList.size
     inner class ViewHolder(val binding: StoreItemWearingBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(item: DataXXXX){
-//            if (item.isSelected==true) {
-//                binding.itemUsingbgImg.setImageResource(R.drawable.store_wearing_item_r_selected)
-//            } else {
-//                binding.itemUsingbgImg.setImageResource(R.drawable.store_wearingitem_r)
-//            }
+            when(item.itemId){//stone이미지
+                "1e5b03b8-ca6a-11ee-a240-0208b3f73ebc" -> binding.itemUsingitemImg.setImageResource(R.drawable.stone_basic)
+                "39e97a1b-ca6a-11ee-a240-0208b3f73ebc" -> binding.itemUsingitemImg.setImageResource(R.drawable.stone_ruby)
+                "39f48564-ca6a-11ee-a240-0208b3f73ebc" -> binding.itemUsingitemImg.setImageResource(R.drawable.stone_sapphire)
+                "39ff7844-ca6a-11ee-a240-0208b3f73ebc" -> binding.itemUsingitemImg.setImageResource(R.drawable.stone_emerald)
+                "3a09c2a1-ca6a-11ee-a240-0208b3f73ebc" -> binding.itemUsingitemImg.setImageResource(R.drawable.stone_metal)
+                "3a144606-ca6a-11ee-a240-0208b3f73ebc" -> binding.itemUsingitemImg.setImageResource(R.drawable.stone_gold)
+                "3a1f3498-ca6a-11ee-a240-0208b3f73ebc" -> binding.itemUsingitemImg.setImageResource(R.drawable.stone_silver)
+                "3a2a4295-ca6a-11ee-a240-0208b3f73ebc" -> binding.itemUsingitemImg.setImageResource(R.drawable.stone_bronze)
+                else -> binding.itemUsingitemImg.setImageResource(R.drawable.stone_basic)
+            }
             binding.itemUsingbgCircle.setImageResource(R.drawable.store_checkimg)
-            binding.howmuchTv.isInvisible
-            binding.itemUsingitemImg.setImageResource(R.drawable.bell)
+            binding.howmuchTv.text=""
         }
     }
 }
